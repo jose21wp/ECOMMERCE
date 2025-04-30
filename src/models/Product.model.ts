09e2d0f0e7bad5f6a96dcd62ec4e9f923ed94c65
Jose Espinoza <jose21wp@gmail.com>
2025-04-30 17:10:58 -0400

    se modifica el README

---
+++ src/models/Product.model.ts
@@ -0,0 +1,52 @@
+interface Dimensions {
+    width: number;
+    height: number;
+    depth: number;
+}
+
+interface Review {
+    rating: number;
+    comment: string;
+    date: string;
+    reviewerName: string;
+    reviewerEmail: string;
+}
+
+interface MetaData {
+    createdAt: string;
+    updatedAt: string;
+    barcode: string;
+    qrCode: string;
+}
+
+interface Product {
+    id: number;
+    title: string;
+    description: string;
+    category: string;
+    price: number;
+    discountPercentage: number;
+    rating: number;
+    stock: number;
+    tags: string[];
+    brand: string;
+    sku: string;
+    weight: number;
+    dimensions: Dimensions;
+    warrantyInformation: string;
+    shippingInformation: string;
+    availabilityStatus: string;
+    reviews: Review[];
+    returnPolicy: string;
+    minimumOrderQuantity: number;
+    meta: MetaData;
+    thumbnail: string;
+    images: string[];
+}
+
+interface ProductsResponse {
+    products: Product[];
+    total: number;
+    skip: number;
+    limit: number;
+}
